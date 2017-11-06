1d3523a54244805cf89769b45595d28a278faec1
ECasielles <flapspoiler@gmail.com>
2017-11-06 13:34:37 +0100

    Custom style, pojos, and Login design

---
+++ app/src/main/java/com/example/usuario/chronotasker/pojo/Language.java
@@ -0,0 +1,16 @@
+package com.example.usuario.chronotasker.pojo;
+
+/**
+ * Representa los idiomas implementados por la aplicación
+ *
+ * @author Enrique Casielles Lapeira
+ * @version 1.0
+ * @see AppIcon
+ */
+
+public class Language {
+    int _ID;
+    String _name;
+    String _code;
+    AppIcon _flagIcon;
+}
